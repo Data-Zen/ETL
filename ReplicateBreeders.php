72e01e8c50be8d6ff52ff10606e949b7a4d47a22
Paully13x <paulkats@gmail.com>
2016-09-12 14:42:15 -0700

    aaa

--- ReplicateBreeders.php
+++ ReplicateBreeders.php
@@ -6,88 +6,83 @@ $start_total_timer = microtime(true);
 include 'credentials/PBBCredentials.php';
 
 print strftime('%c');
-$connect = pg_connect($PBBModifyCredentials);
-$processname="breaders";
-$sql = "select nvl(lastmodified_dt,'1/1/1900') lastmodified_dt from buypuppy_test.processhistory where processname='$processname';";
+$connect     = pg_connect($PBBModifyCredentials);
+$processname = "breaders";
+$sql         = "select nvl(lastmodified_dt,'1/1/1900') lastmodified_dt from buypuppy_test.processhistory where processname='$processname';";
 echo "\n*******StartQuery\n" . $sql . "\n*******EndQuery\n";
 $resulttotal = pg_query($connect, $sql);
 if ($row = pg_fetch_array($resulttotal)) {
-        $lastmodified_dt= $row["lastmodified_dt"];
-    }
-    else
-    {
-        $lastmodified_dt="1/1/1900";
-    }
-    ;
+    $lastmodified_dt = $row["lastmodified_dt"];
+} else {
+    $lastmodified_dt = "1/1/1900";
+}
+;
 
 
 
-$sql="select * from buypuppy_test.breaders where lastmodified_dt>'$lastmodified_dt' order by lastmodified_dt asc limit 100";
+$sql = "select * from buypuppy_test.breaders where lastmodified_dt>'$lastmodified_dt' order by lastmodified_dt asc limit 100";
 echo "\n*******StartQuery\n" . $sql . "\n*******EndQuery\n";
 $resulttotal = pg_query($connect, $sql);
 while ($row = pg_fetch_array($resulttotal)) {
-    $id= $row["id"];
-    $company_name= $row["company_name"];
-    $breader_status_id= $row["breader_status_id"];
-    $lastmodified_dt= $row["lastmodified_dt"];
-
-        $sql="select 1 as resultid from buypuppy_test.geodesic_userdata where id=$id";
-            echo "\n*******StartQuery\n" . $sql . "\n*******EndQuery\n";
-            $result = pg_query($connect, $sql);
-            if (pg_num_rows($result) > 0 ){
-
-                $sql="update buypuppy_test.geodesic_userdata set id=$id,company_name='".addslashes($company_name)."' where id=$id";
-
-            }
-            else
-            {
-                $sql="insert into buypuppy_test.geodesic_userdata (id,company_name) values ($id,'".addslashes($company_name)."')";
-            }
+    $id                = $row["id"];
+    $company_name      = $row["company_name"];
+    $breader_status_id = $row["breader_status_id"];
+    $lastmodified_dt   = $row["lastmodified_dt"];
+    
+    $sql = "select 1 as resultid from buypuppy_test.geodesic_userdata where id=$id";
+    echo "\n*******StartQuery\n" . $sql . "\n*******EndQuery\n";
+    $result = pg_query($connect, $sql);
+    if (pg_num_rows($result) > 0) {
         
-
-        echo "\n*******StartQuery\n" . $sql . "\n*******EndQuery\n";
-        $result = pg_query($connect, $sql);
-
-
-
-        $sql="select 1 as resultid from buypuppy_test.geodesic_logins where id=$id";
-            echo "\n*******StartQuery\n" . $sql . "\n*******EndQuery\n";
-            $result = pg_query($connect, $sql);
-            if (pg_num_rows($result) > 0 ){
-
-                $sql="update buypuppy_test.geodesic_logins set id=$id,status=$breader_status_id where id=$id";
-
-            }
-            else
-            {
-                $sql="insert into buypuppy_test.geodesic_logins (id,status) values ($id,$breader_status_id)";
-            }
+        $sql = "update buypuppy_test.geodesic_userdata set id=$id,company_name='" . addslashes($company_name) . "' where id=$id";
         
-
-        echo "\n*******StartQuery\n" . $sql . "\n*******EndQuery\n";
-        $result = pg_query($connect, $sql);
-        $rowsaffected = pg_affected_rows($result);
-
-    };
-
-if ($rowsaffected > 0) {
+    } else {
+        $sql = "insert into buypuppy_test.geodesic_userdata (id,company_name) values ($id,'" . addslashes($company_name) . "')";
+    }
+    
+    
+    echo "\n*******StartQuery\n" . $sql . "\n*******EndQuery\n";
+    $result = pg_query($connect, $sql);
+    
+    
+    
+    $sql = "select 1 as resultid from buypuppy_test.geodesic_logins where id=$id";
+    echo "\n*******StartQuery\n" . $sql . "\n*******EndQuery\n";
+    $result = pg_query($connect, $sql);
+    if (pg_num_rows($result) > 0) {
+        
+        $sql = "update buypuppy_test.geodesic_logins set id=$id,status=$breader_status_id where id=$id";
+        
+    } else {
+        $sql = "insert into buypuppy_test.geodesic_logins (id,status) values ($id,$breader_status_id)";
+    }
+    
+    
+    echo "\n*******StartQuery\n" . $sql . "\n*******EndQuery\n";
+    $result       = pg_query($connect, $sql);
+    $rowsaffected = pg_affected_rows($result);
+    
+    
+    
+    if ($rowsaffected > 0) {
         $sql = "update buypuppy_test.processhistory set lastmodified_dt='$lastmodified_dt'  where processname='$processname';";
-
+        
         echo "\n*******StartQuery\n" . $sql . "\n*******EndQuery\n";
-        $result = pg_query($connect, $sql);
+        $result       = pg_query($connect, $sql);
         $rowsaffected = pg_affected_rows($result);
         
-            if ($rowsaffected == 0) { 
-
-                $sql = "insert into buypuppy_test.processhistory (processname,lastmodified_dt) values ('$processname','$lastmodified_dt');";
-                echo "\n*******StartQuery\n" . $sql . "\n*******EndQuery\n";
-                $result = pg_query($connect, $sql);
-
-              }
+        if ($rowsaffected == 0) {
+            
+            $sql = "insert into buypuppy_test.processhistory (processname,lastmodified_dt) values ('$processname','$lastmodified_dt');";
+            echo "\n*******StartQuery\n" . $sql . "\n*******EndQuery\n";
+            $result = pg_query($connect, $sql);
+            
+        }
+    }
+    
+    
 }
-
-
-
+;
 
 
 
@@ -95,8 +90,8 @@ if ($rowsaffected > 0) {
 
 
 print strftime('%c');
-     $end11 = round((microtime(true) - $start_total_timer),2);
-        echo "\n=============================== Elapsed Total Time " . round($end11 /60,1) ." Minutes =============================== \n";
+$end11 = round((microtime(true) - $start_total_timer), 2);
+echo "\n=============================== Elapsed Total Time " . round($end11 / 60, 1) . " Minutes =============================== \n";
 
 
 ?>
