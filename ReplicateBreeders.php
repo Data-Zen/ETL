fbf55b79bb25b1345ed08141f21fed893c020807
Paully13x <paulkats@gmail.com>
2016-09-12 14:26:14 -0700

    aaa

--- ReplicateBreeders.php
+++ ReplicateBreeders.php
@@ -8,15 +8,22 @@ include 'credentials/PBBCredentials.php';
 print strftime('%c');
 $connect = pg_connect($PBBModifyCredentials);
 $processname="breaders";
-$sql = "select lastmodified_dt from buypuppy_test.processhistory where processname=$processname;"
+$sql = "select nvl(lastmodified_dt,'1/1/1900') lastmodified_dt from buypuppy_test.processhistory where processname='$processname';";
 echo "\n*******StartQuery\n" . $sql . "\n*******EndQuery\n";
 $resulttotal = pg_query($connect, $sql);
-while ($row = pg_fetch_array($resulttotal)) {
-    $lastmodified_dt= $row["lastmodified_dt"];
-    };
+if ($row = pg_fetch_array($resulttotal)) {
+        $lastmodified_dt= $row["lastmodified_dt"];
+    }
+    else
+    {
+        $lastmodified_dt="1/1/1900";
+    }
+    ;
 
-$sql="select * from buypuppy_test.breaders where lastmodified_dt>$lastmodified_dt order by lastmodified_dt asc limit 100"
 
+
+$sql="select * from buypuppy_test.breaders where lastmodified_dt>'$lastmodified_dt' order by lastmodified_dt asc limit 100";
+echo "\n*******StartQuery\n" . $sql . "\n*******EndQuery\n";
 $resulttotal = pg_query($connect, $sql);
 while ($row = pg_fetch_array($resulttotal)) {
     $id= $row["id"];
@@ -24,12 +31,34 @@ while ($row = pg_fetch_array($resulttotal)) {
     $breader_status_id= $row["breader_status_id"];
     $lastmodified_dt= $row["lastmodified_dt"];
 
-        $sql="insert into buypuppy_test.geodesic_userdata (id,company_name) values ($id,$company_name)"
+        $sql="insert into buypuppy_test.geodesic_userdata (id,company_name) values ($id,'".addslashes($company_name)."')";
+        echo "\n*******StartQuery\n" . $sql . "\n*******EndQuery\n";
         $result = pg_query($connect, $sql);
-        $sql="insert into buypuppy_test.geodesic_logins (id,status) values ($id,$status)"
+        $sql="insert into buypuppy_test.geodesic_logins (id,status) values ($id,$breader_status_id)";
+        echo "\n*******StartQuery\n" . $sql . "\n*******EndQuery\n";
         $result = pg_query($connect, $sql);
+        $rowsaffected = pg_affected_rows($result);
 
     };
+
+if ($rowsaffected > 0) {
+        $sql = "update buypuppy_test.processhistory set lastmodified_dt='$lastmodified_dt'  where processname='$processname';";
+
+        echo "\n*******StartQuery\n" . $sql . "\n*******EndQuery\n";
+        $result = pg_query($connect, $sql);
+        $rowsaffected = pg_affected_rows($result);
+        
+            if ($rowsaffected == 0) { 
+
+                $sql = "insert into buypuppy_test.processhistory (processname,lastmodified_dt) values ('$processname','$lastmodified_dt');";
+                echo "\n*******StartQuery\n" . $sql . "\n*******EndQuery\n";
+                $result = pg_query($connect, $sql);
+
+              }
+}
+
+
+
 
 
 
