de2d77c1bf30eddad908fc05b499cc17c2b47b11
Paully13x <paulkats@gmail.com>
2016-09-12 21:33:07 -0700

    ddd

--- ReplicateBreeders.php
+++ ReplicateBreeders.php
@@ -7,9 +7,9 @@ include 'credentials/PBBCredentials.php';
 
 print strftime('%c');
 $connect     = pg_connect($PBBModifyCredentials);
-$processname = "breaders";
+$processname = "breeders";
 $sql         = "select nvl(lastmodified_dt,'1/1/1900') lastmodified_dt from buypuppy_test.processhistory where processname='$processname';";
-echo "\n*******StartQuery\n" . $sql . "\n*******EndQuery\n";
+echo "\n*******StartQuery To Get ProcessHistory\n" . $sql . "\n*******EndQuery\n";
 $resulttotal = pg_query($connect, $sql);
 if ($row = pg_fetch_array($resulttotal)) {
     $lastmodified_dt = $row["lastmodified_dt"];
@@ -20,61 +20,89 @@ if ($row = pg_fetch_array($resulttotal)) {
 
 
 
-$sql = "select * from buypuppy_test.breaders where lastmodified_dt>'$lastmodified_dt' order by lastmodified_dt asc limit 100";
-echo "\n*******StartQuery\n" . $sql . "\n*******EndQuery\n";
+$sql = "select * from buypuppy_test.$processname where lastmodified_dt>'$lastmodified_dt' order by lastmodified_dt asc limit 100";
+echo "\n*******StartQuery To Get New Data From SourceTable\n" . $sql . "\n*******EndQuery\n";
 $resulttotal = pg_query($connect, $sql);
 while ($row = pg_fetch_array($resulttotal)) {
     $id                = $row["id"];
     $company_name      = $row["company_name"];
-    $breader_status_id = $row["breader_status_id"];
+    $breeder_status_id = $row["breeder_status_id"];
     $lastmodified_dt   = $row["lastmodified_dt"];
     
     $sql = "select 1 as resultid from buypuppy_test.geodesic_userdata where id=$id";
-    echo "\n*******StartQuery\n" . $sql . "\n*******EndQuery\n";
+    echo "\n*******StartQuery To Check If Row Already Exists\n" . $sql . "\n*******EndQuery\n";
     $result = pg_query($connect, $sql);
+
     if (pg_num_rows($result) > 0) {
         
         $sql = "update buypuppy_test.geodesic_userdata set id=$id,company_name='" . addslashes($company_name) . "' where id=$id";
+        $operation_type="Update";
         
     } else {
         $sql = "insert into buypuppy_test.geodesic_userdata (id,company_name) values ($id,'" . addslashes($company_name) . "')";
+        $operation_type="Insert";
     }
     
     
-    echo "\n*******StartQuery\n" . $sql . "\n*******EndQuery\n";
+    echo "\n*******StartQuery To Upsert The Record \n" . $sql . "\n*******EndQuery\n";
+    $result = pg_query($connect, $sql);
+
+    $numrows = 0;
+    $numrows = pg_affected_rows($result);
+    $sql="INSERT INTO dev.buypuppy_test.processhistory_extended 
+        select '$processname'
+       , '$processname'
+       , 'geodesic_userdata'
+       , $numrows
+       , $id
+       , '$operation_type'
+       , getdate();";
+    echo "\n*******StartQuery To Log Extended Details \n" . $sql . "\n*******EndQuery\n";
     $result = pg_query($connect, $sql);
     
-    
-    
     $sql = "select 1 as resultid from buypuppy_test.geodesic_logins where id=$id";
-    echo "\n*******StartQuery\n" . $sql . "\n*******EndQuery\n";
+    echo "\n*******StartQuery  To Check If Row Already Exists\n" . $sql . "\n*******EndQuery\n";
     $result = pg_query($connect, $sql);
     if (pg_num_rows($result) > 0) {
         
-        $sql = "update buypuppy_test.geodesic_logins set id=$id,status=$breader_status_id where id=$id";
+        $sql = "update buypuppy_test.geodesic_logins set id=$id,status=$breeder_status_id where id=$id";
+        $operation_type="Update";
         
     } else {
-        $sql = "insert into buypuppy_test.geodesic_logins (id,status) values ($id,$breader_status_id)";
+        $sql = "insert into buypuppy_test.geodesic_logins (id,status) values ($id,$breeder_status_id)";
+        $operation_type="Insert";
     }
     
     
-    echo "\n*******StartQuery\n" . $sql . "\n*******EndQuery\n";
+    echo "\n*******StartQuery To Upsert The Record\n" . $sql . "\n*******EndQuery\n";
     $result       = pg_query($connect, $sql);
-    $rowsaffected = pg_affected_rows($result);
+
+    $numrows = 0;
+    $numrows = pg_affected_rows($result);
+    $sql="INSERT INTO dev.buypuppy_test.processhistory_extended 
+        select '$processname'
+       , '$processname'
+       , 'geodesic_logins'
+       , $numrows
+       , $id
+       , '$operation_type'
+       , getdate();";
+    echo "\n*******StartQuery To Log Extended Details\n" . $sql . "\n*******EndQuery\n";
+    $result = pg_query($connect, $sql);
     
     
     
-    if ($rowsaffected > 0) {
+    if ($numrows > 0) {
         $sql = "update buypuppy_test.processhistory set lastmodified_dt='$lastmodified_dt'  where processname='$processname';";
         
-        echo "\n*******StartQuery\n" . $sql . "\n*******EndQuery\n";
+        echo "\n*******StartQuery To Update ProcessHistory On Latest LastModifed Date\n" . $sql . "\n*******EndQuery\n";
         $result       = pg_query($connect, $sql);
         $rowsaffected = pg_affected_rows($result);
         
         if ($rowsaffected == 0) {
             
             $sql = "insert into buypuppy_test.processhistory (processname,lastmodified_dt) values ('$processname','$lastmodified_dt');";
-            echo "\n*******StartQuery\n" . $sql . "\n*******EndQuery\n";
+            echo "\n*******StartQuery Insert Into ProcessHistory if First Time\n" . $sql . "\n*******EndQuery\n";
             $result = pg_query($connect, $sql);
             
         }
@@ -83,9 +111,6 @@ while ($row = pg_fetch_array($resulttotal)) {
     
 }
 ;
-
-
-
 
 
 
